04b4282b3aac025e05967e1249fde2fa1d331111
abneerodriggo <abneerodriggo@gmail.com>
2024-07-12 20:00:20 -0300

    Algumas atividades das aulas relacionadas à estruturas de controle de fluxo: For

---
+++ controle-fluxo/src/ExemploFor_EmArray_Each.java
@@ -0,0 +1,10 @@
+public class ExemploFor_EmArray_Each {
+    public static void main(String[] args) {
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+
+        //Forma abreviada
+        for(String aluno : alunos) {
+            System.out.println("O nome do aluno é: " + aluno);
+        }
+    }
+}
